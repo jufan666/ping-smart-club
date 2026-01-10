00f8e330a4fcd120c4cefe848cbb84e2bc505dce
jufan <857937269@qq.com>
2026-01-10 11:17:10 +0800

    ping-smart-club-boot

---
+++ ping-smart-club-web-boot/src/main/java/org/pingpongapp/pingsmartclubwebboot/PingSmartClubWebBootApplication.java
@@ -0,0 +1,13 @@
+package org.pingpongapp.pingsmartclubwebboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingSmartClubWebBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingSmartClubWebBootApplication.class, args);
+    }
+
+}
---
+++ ping-smart-club-web-boot/src/test/java/org/pingpongapp/pingsmartclubwebboot/PingSmartClubWebBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pingpongapp.pingsmartclubwebboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingSmartClubWebBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
